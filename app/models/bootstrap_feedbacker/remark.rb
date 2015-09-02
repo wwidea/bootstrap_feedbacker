5fd519883c898e9062373802a829daed90a7c87f
David Gross <david.gross@daggerweb.org>
2015-09-02 15:51:27 -0600

    added setting for user_class

--- app/models/bootstrap_feedbacker/remark.rb
+++ app/models/bootstrap_feedbacker/remark.rb
@@ -1,6 +1,6 @@
 module BootstrapFeedbacker
   class Remark < ActiveRecord::Base
-    belongs_to :user
+    belongs_to :user, class_name: BootstrapFeedbacker::SETTINGS.user_class
 
     def user_name
       return user.send(BootstrapFeedbacker::SETTINGS.user_name_method)
--- lib/bootstrap_feedbacker/settings.rb
+++ lib/bootstrap_feedbacker/settings.rb
@@ -1,10 +1,11 @@
 module BootstrapFeedbacker
   class Settings
-    attr_accessor :email_to, :email_prefix, :user_name_method
+    attr_accessor :email_to, :email_prefix, :user_name_method, :user_class
     
     def initialize
       self.email_to           = ''
       self.email_prefix       = '[FEEDBACKER]'
+      self.user_class         = "User"
       self.user_name_method   = :name
     end
     
