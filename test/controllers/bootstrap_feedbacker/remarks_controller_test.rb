364a1e67041f99e3d37c719ad396eb81e839e820
David Gross <david.gross@daggerweb.org>
2016-11-23 13:38:37 -0700

    update bootstrap feedbacker to support rails5

--- test/controllers/bootstrap_feedbacker/remarks_controller_test.rb
+++ test/controllers/bootstrap_feedbacker/remarks_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 module BootstrapFeedbacker
-  class RemarksControllerTest < ActionController::TestCase
+  class RemarksControllerTest < ActionDispatch::IntegrationTest
     setup do
       @remark = bootstrap_feedbacker_remarks(:remark)
       @routes = Engine.routes
@@ -9,7 +9,7 @@ module BootstrapFeedbacker
 
     test "should create remark" do
       assert_difference('Remark.count') do
-        post :create, remark: { content: @remark.content }, format: 'js'
+        post @routes.url_helpers.remarks_path, params: { remark: { content: @remark.content }, format: :js }
       end
       assert_response 200
     end
