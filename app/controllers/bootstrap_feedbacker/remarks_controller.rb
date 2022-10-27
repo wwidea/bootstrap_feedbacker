29762554c88df09f593579ab88efb24285f32556
Aaron Baldwin <baldwina@brightwayslearning.org>
2022-10-27 12:20:35 -0600

    add new action

--- app/controllers/bootstrap_feedbacker/remarks_controller.rb
+++ app/controllers/bootstrap_feedbacker/remarks_controller.rb
@@ -2,14 +2,15 @@
 
 module BootstrapFeedbacker
   class RemarksController < ApplicationController
+    def new
+      render
+    end
+
     def create
       @remark = Remark.new(remark_params.merge(user_id: current_user.id))
       @remark.source_url = request.env['HTTP_REFERER'] || "no referrer"
       if @remark.save
         FeedbackMailer.feedback(@remark).deliver_now
-        respond_to do |format|
-          format.js
-        end
       end
     end
 
