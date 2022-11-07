4a7936c03384bac1189eb19a80f68b815784777f
Aaron Baldwin <baldwina@brightwayslearning.org>
2022-11-07 10:52:23 -0700

    update turbo streams from controller

--- app/controllers/bootstrap_feedbacker/remarks_controller.rb
+++ app/controllers/bootstrap_feedbacker/remarks_controller.rb
@@ -4,20 +4,30 @@ module BootstrapFeedbacker
   class RemarksController < ApplicationController
     def new
       @remark = BootstrapFeedbacker::Remark.new
+      render_update(SETTINGS.modal_id, partial: 'new')
     end
 
     def create
-      @remark = Remark.new(remark_params.merge(user_id: current_user.id))
-      @remark.source_url = request.env['HTTP_REFERER'] || "no referrer"
+      @remark = Remark.new(remark_params)
       if @remark.save
         FeedbackMailer.feedback(@remark).deliver_now
+        render_update(SETTINGS.modal_content_id, partial: 'thank_you')
+      else
+        render_update(SETTINGS.modal_content_id, partial: 'error', status: :unprocessable_entity)
       end
     end
 
     private
 
     def remark_params
-      params.require(:remark).permit(:content)
+      params.require(:remark).permit(:content).merge(
+        user_id:    current_user.id,
+        source_url: request.env['HTTP_REFERER'] || 'no referrer'
+      )
+    end
+
+    def render_update(dom_id, partial:, status: :ok)
+      render(turbo_stream: turbo_stream.update(dom_id, partial: partial), status:)
     end
   end
 end
--- test/system/bootstrap_feedbacker/remarks_test.rb
+++ test/system/bootstrap_feedbacker/remarks_test.rb
@@ -2,11 +2,24 @@ require "application_system_test_case"
 
 class RemarksTest < ApplicationSystemTestCase
   test "creating a remark" do
-    visit dashboard_path
-    click_link 'Feedback'
-    assert_selector 'div', text: "What's on your mind?"
+    open_feedback_form
+    assert_selector 'div', text: I18n.t('bootstrap_feedbacker.remarks.form.greeting')
     fill_in "remark[content]", with: 'I need help!'
     click_on 'Submit Feedback'
-    assert_selector 'div', text: 'Thank you for your feedback!'
+    assert_selector 'div', text: I18n.t('bootstrap_feedbacker.remarks.thank_you.message')
+  end
+
+  test "failing to create a remark" do
+    BootstrapFeedbacker::Remark.any_instance.expects(:valid?).returns(false)
+    open_feedback_form
+    click_on 'Submit Feedback'
+    assert_selector 'div', text: I18n.t('bootstrap_feedbacker.remarks.error.message')
+  end
+
+  private
+
+  def open_feedback_form
+    visit dashboard_path
+    click_link 'Feedback'
   end
 end
