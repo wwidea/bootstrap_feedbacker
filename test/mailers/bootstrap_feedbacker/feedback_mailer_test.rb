caa0434fd34252a43be91abcf56b9d5b8f76656b
Aaron Baldwin <baldwina@brightwayslearning.org>
2023-02-02 10:09:08 -0700

    change parentheses to brackets

--- test/mailers/bootstrap_feedbacker/feedback_mailer_test.rb
+++ test/mailers/bootstrap_feedbacker/feedback_mailer_test.rb
@@ -7,9 +7,9 @@ module BootstrapFeedbacker
     test "should send parent feedback" do
       mail = BootstrapFeedbacker::FeedbackMailer.feedback(remark)
       assert_equal "[FEEDBACKER] Feedback Notification",  mail.subject
-      assert_equal %w(feedback@example.com),              mail.to
-      assert_equal %w(feedback@example.com),              mail.from
-      assert_equal %w(tiffany@example.com),               mail.reply_to
+      assert_equal %w[feedback@example.com],              mail.to
+      assert_equal %w[feedback@example.com],              mail.from
+      assert_equal %w[tiffany@example.com],               mail.reply_to
       assert_match remark.content,                        mail.body.encoded
       assert_match remark.source_url,                     mail.body.encoded
     end
