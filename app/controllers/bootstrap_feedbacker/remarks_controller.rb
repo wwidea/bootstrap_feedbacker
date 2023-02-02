8e080fd7b3b2ca001323c5563edea95d7c19c5bd
Aaron Baldwin <baldwina@brightwayslearning.org>
2023-02-02 11:52:08 -0700

    use double quotes

--- app/controllers/bootstrap_feedbacker/remarks_controller.rb
+++ app/controllers/bootstrap_feedbacker/remarks_controller.rb
@@ -4,16 +4,16 @@ module BootstrapFeedbacker
   class RemarksController < ApplicationController
     def new
       @remark = BootstrapFeedbacker::Remark.new
-      render_update(SETTINGS.modal_id, partial: 'new')
+      render_update(SETTINGS.modal_id, partial: "new")
     end
 
     def create
       @remark = Remark.new(remark_params)
       if @remark.save
         FeedbackMailer.feedback(@remark).deliver_now
-        render_update(SETTINGS.modal_content_id, partial: 'thank_you')
+        render_update(SETTINGS.modal_content_id, partial: "thank_you")
       else
-        render_update(SETTINGS.modal_content_id, partial: 'error', status: :unprocessable_entity)
+        render_update(SETTINGS.modal_content_id, partial: "error", status: :unprocessable_entity)
       end
     end
 
@@ -22,7 +22,7 @@ module BootstrapFeedbacker
     def remark_params
       params.require(:remark).permit(:content).merge(
         user_id:    current_user.id,
-        source_url: request.env['HTTP_REFERER'] || 'no referrer'
+        source_url: request.env["HTTP_REFERER"] || "no referrer"
       )
     end
 
